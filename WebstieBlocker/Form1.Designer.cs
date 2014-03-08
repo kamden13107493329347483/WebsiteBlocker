3ffdf905b4e0ceb9914ea26b36d0c521d3699d15
fjam <fahad.jameel02@gmail.com>
2014-03-08 14:24:53 -0500

    Initial Upload

---
+++ WebstieBlocker/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace WebstieBlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.UnBlock = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Block = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.UnBlock.SuspendLayout();
+            this.Block.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UnBlock
+            // 
+            this.UnBlock.Controls.Add(this.button4);
+            this.UnBlock.Controls.Add(this.button3);
+            this.UnBlock.Controls.Add(this.richTextBox1);
+            this.UnBlock.Controls.Add(this.button2);
+            this.UnBlock.Location = new System.Drawing.Point(4, 22);
+            this.UnBlock.Name = "UnBlock";
+            this.UnBlock.Padding = new System.Windows.Forms.Padding(3);
+            this.UnBlock.Size = new System.Drawing.Size(359, 142);
+            this.UnBlock.TabIndex = 1;
+            this.UnBlock.Text = "UnBlock";
+            this.UnBlock.UseVisualStyleBackColor = true;
+            this.UnBlock.Click += new System.EventHandler(this.UnBlock_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(87, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(269, 130);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Unblock";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Block
+            // 
+            this.Block.Controls.Add(this.label1);
+            this.Block.Controls.Add(this.label7);
+            this.Block.Controls.Add(this.label2);
+            this.Block.Controls.Add(this.label6);
+            this.Block.Controls.Add(this.textBox1);
+            this.Block.Controls.Add(this.textBox2);
+            this.Block.Controls.Add(this.label5);
+            this.Block.Controls.Add(this.label4);
+            this.Block.Controls.Add(this.button1);
+            this.Block.Controls.Add(this.label3);
+            this.Block.Location = new System.Drawing.Point(4, 22);
+            this.Block.Name = "Block";
+            this.Block.Padding = new System.Windows.Forms.Padding(3);
+            this.Block.Size = new System.Drawing.Size(359, 142);
+            this.Block.TabIndex = 0;
+            this.Block.Text = "Block";
+            this.Block.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Site To Block:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "(without http://www.)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Batch Code";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "(ex. google.com)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 32);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 105);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "http://etechforum.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "By Fjam";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(28, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Block";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Generated:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Block);
+            this.tabControl1.Controls.Add(this.UnBlock);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(367, 168);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(359, 142);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Instructions";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(359, 142);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Defalut Code";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(364, 167);
+            this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Website Blocker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.UnBlock.ResumeLayout(false);
+            this.Block.ResumeLayout(false);
+            this.Block.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage UnBlock;
+        private System.Windows.Forms.TabPage Block;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button4;
+
+    }
+}
+
---
+++ WebstieBlocker/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace WebstieBlocker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Hpath = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process myProcess = new Process();
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter a valid URL");
+            }
+            else
+            {
+                string batFile;
+                batFile = genBat(textBox1.Text);
+                string path = "C:\\Windows\\System32\\drivers\\etc\\blocksite.bat";
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine(@batFile);
+                }
+
+                Process.Start(path).WaitForExit();
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+         }
+
+        public string genBat(string site)
+        {
+            string code;
+            string Fsite;
+                
+            Fsite = "www."+ site;
+            
+            textBox2.Text = "echo off\r\n";
+            textBox2.Text += "cd C:\\Windows\\System32\\drivers\\etc\r\n";
+            textBox2.Text += "echo. >> \"hosts\"\r\n";
+            textBox2.Text += "echo 127.0.0.1 " + Fsite + " >> \"hosts\"\r\n";
+            textBox2.Text += "echo 127.0.0.1 " + site + ">> \"hosts\""; 
+            
+            code = textBox2.Text;
+
+            return code;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UnBlock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText(Hpath);     
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //StreamWriter sw = new StreamWriter(Hpath);
+            //sw.Write(richTextBox1.Lines);
+            richTextBox1.SaveFile(Hpath, RichTextBoxStreamType.PlainText);
+            //sw.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox2.ReadOnly = true;
+            richTextBox2.Text = "Instructions:";
+            richTextBox2.Text += "\nBLOCK:\n1. Enter desired URL without http:// and/or www. ";
+            richTextBox2.Text += "\n2. Now press the Block button.";
+            richTextBox2.Text += "\n3. Check if site is blocked.\n";
+
+            richTextBox2.Text += "\nUNBLOCK:";
+            richTextBox2.Text += "\n1. Press ope.n";
+            richTextBox2.Text += "\n2. Sroll to the bottom.";
+            richTextBox2.Text += "\n3. ONLY ERASE the 127.0.0.1 followed by site you want to unblock.     Ex. 127.0.0.1 google.com \n      127.0.0.1 www.google.com";
+            richTextBox2.Text += "\n4. Press Save.";
+            richTextBox2.Text += "\n\nNote: If you accidently mess up your file and save it, press the \"Default Code\" button and copy&paste that.";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+
+    }
+}
+
